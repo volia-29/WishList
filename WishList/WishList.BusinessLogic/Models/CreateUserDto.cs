ec6b5c5b574e281076cc5e5ace5f5ea69680dc39
Volodymyr Drahun <volodymyr.drahun@eleks.com>
2024-11-04 15:58:27 +0200

    test pp

--- WishList/WishList.BusinessLogic/Models/CreateUserDto.cs
+++ WishList/WishList.BusinessLogic/Models/CreateUserDto.cs
@@ -6,8 +6,5 @@ namespace WishList.BusinessLogic.Models
     {
         [Required]
         public string Name { get; set; }
-
-        [Required]
-        public string Password { get; set; }
     }
 }
